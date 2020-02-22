b95d44d7d06866bfa95d0231815f58b2677e2280
ashu2163 <ashutoshmehta65@gmail.com>
2020-02-22 11:31:44 -0600

    fixed login registration page

--- SimpleRegistrationandLoginApplication/app/src/main/java/com/myApp/simpleregistrationandloginapplication/MainActivity.java
+++ SimpleRegistrationandLoginApplication/app/src/main/java/com/myApp/simpleregistrationandloginapplication/MainActivity.java
@@ -1,5 +1,5 @@
 package com.razormist.simpleregistrationandloginapplication;
-
+//package com.myApp.simpleregistrationandloginapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,20 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper databaseHelper;
 
     EditText et_username, et_password, et_cpassword,et_fname,et_lname,et_email,et_address;
     Button btn_register, btn_login;
+
+    RadioGroup rg;
+    RadioButton rb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         et_cpassword = (EditText)findViewById(R.id.et_cpassword);
         et_email = (EditText)findViewById(R.id.et_email);
         et_address = (EditText)findViewById(R.id.et_address);
+        rg = (RadioGroup) findViewById(R.id.et_rgroup);
+
+
         btn_register = (Button)findViewById(R.id.btn_register);
         btn_login = (Button)findViewById(R.id.btn_login);
 
@@ -51,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int selectedId = rg.getCheckedRadioButtonId();
+                rb=(RadioButton)findViewById(selectedId);
+                String role=rb.getText().toString();
 
                 String fname = et_fname.getText().toString();
                 String lname = et_lname.getText().toString();
@@ -61,13 +65,17 @@ public class MainActivity extends AppCompatActivity {
                 String address = et_address.getText().toString();
 
                 if(username.equals("") || password.equals("") || confirm_password.equals("")){
+                    //Toast.makeText(getApplicationContext(), role, Toast.LENGTH_SHORT).show();
                     Toast.makeText(getApplicationContext(), "Fields Required", Toast.LENGTH_SHORT).show();
                 }else{
+                    //Toast.makeText(getApplicationContext(), role, Toast.LENGTH_SHORT).show();
                     if(password.equals(confirm_password)){
                         Boolean checkusername = databaseHelper.CheckUsername(username);
                         if(checkusername == true){
-                            Boolean insert = databaseHelper.Insert(fname,lname,username, password,email,address);
+                            Boolean insert = databaseHelper.Insert(fname,lname,username, password, email, address, role);
+
                             if(insert == true){
+
                                 Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_SHORT).show();
                                 et_fname.setText("");
                                 et_lname.setText("");
---
+++ SimpleRegistrationandLoginApplication/app/src/main/java/com/myApp/simpleregistrationandloginapplication/ManagerHomeActivity.java
@@ -0,0 +1,16 @@
+//package com.myApp.simpleregistrationandloginapplication;
+package com.razormist.simpleregistrationandloginapplication;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.razormist.simpleregistrationandloginapplication.R;
+
+public class ManagerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_managerhome);
+
+    }
+}
